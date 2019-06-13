918beb0a926555e37bd82019db7e04e4a5c43287
luius321103 <49661134+luius321103@users.noreply.github.com>
2019-06-13 19:04:28 -0300

    Utilizando o Math.pow

---
+++ potenciacao.java
@@ -0,0 +1,13 @@
+package javaapplication1;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+       
+        double base = 3;
+        double expoente = 3;
+        double resultado = Math.pow(base,expoente);
+        System.out.println(resultado);
+    }
+    
+}
